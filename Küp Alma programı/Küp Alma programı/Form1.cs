54b021d393240239c02dab78a63e468f8b8f3e32
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:27:06 +0300

    Add files via upload

---
+++ Küp Alma programı/Küp Alma programı/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Küp_Alma_programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double küp;
+
+            küp = Convert.ToDouble(textBox1.Text);
+
+            küp = küp * küp * küp;
+            label2.Text = küp.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
